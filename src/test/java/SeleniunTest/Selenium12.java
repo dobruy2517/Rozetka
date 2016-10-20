5406999d7e311ab899b32cf1fb51882c3ec997b9
Ihor Dobrovolskyi <Ihor.dobrovolskyi@gmail.com>
2016-10-20 22:08:34 +0300

    PageObject correction

--- src/test/java/SeleniunTest/Selenium12.java
+++ src/test/java/SeleniunTest/Selenium12.java
@@ -1,6 +1,8 @@
 package SeleniunTest;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,20 +12,30 @@ import packageTest.FrontPage;
 import java.util.concurrent.TimeUnit;
 
 public class Selenium12 {
-    @Test
-    public void searchForProduct() {
+    static WebDriver driver;
+
+    @Before
+    public void preCondition() {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
 
+    @After
+    public void postCondition() {
+        driver.quit();
+    }
+
+    @Test
+    public void searchForProduct() {
         FrontPage frontPage = new FrontPage(driver);
         frontPage.open();
         frontPage.enterSearchWord("Дюбель");
         frontPage.clickSearchButton();
         String actualResult = frontPage.element();
         Assert.assertEquals(actualResult.contains("дюбель"), true);
-        driver.quit();
+
 
     }
 }

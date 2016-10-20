6e4f5ffd6bad03ae118c1012625433c8ebb66c75
Ihor Dobrovolskyi <Ihor.dobrovolskyi@gmail.com>
2016-10-20 21:11:08 +0300

    output all products

--- src/test/java/SeleniunTest/RozetkaTest.java
+++ src/test/java/SeleniunTest/RozetkaTest.java
@@ -4,11 +4,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class RozetkaTest {
+
     @Test
     public void goTo() {
         System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
@@ -17,9 +20,13 @@ public class RozetkaTest {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://rozetka.com.ua");
         driver.findElement(By.cssSelector("a[data-title=\"Смартфоны\"]")).click();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.findElement(By.name("goods_list"));
-        String actualResult = driver.findElement(By.xpath("//*[@id=\"block_with_goods\"]/div[1]/div[3]/div[1]/div/div[1]/div[2]/div[4]/a")).getText();
-        Assert.assertEquals(actualResult.contains("Samsung"), true);
+        List<WebElement> actualResult = driver.findElements(By.cssSelector("div[name=\"promotions_catalog_tile\"]+div>a"));
+        for (WebElement i : actualResult) {
+            System.out.println(i.getText());
+        }
+//        Assert.assertEquals(actualResult.contains("Samsung"), true);
         driver.quit();
     }
 }
